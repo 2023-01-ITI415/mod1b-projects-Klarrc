29ddc7dd297cbd7f1eb50df8537cab069b420ea8
Klarrc <kevindanaclark@gmail.com>
2023-02-06 18:25:59 -0500

    Score/HighScore
    Created elements of scripts that display the score incrementally, display the high score, and save the high score for the next time that the game is booted up.

--- Assets/01-Apple Picker/Scripts/Basket.cs
+++ Assets/01-Apple Picker/Scripts/Basket.cs
@@ -1,13 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Basket : MonoBehaviour
 {
+    [Header("Set Dynamically")]
+    public Text         scoreGT;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+   void Start() {
+        // Find a reference to the ScoreCounter
+ 
+        GameObject scoreGO = GameObject.Find("ScoreCounter");
+        // Get the Text Component of that GameO
+        scoreGT = scoreGO.GetComponent<Text>();
+        // Set the starting number of points to
+        scoreGT.text = "0";
     }
 
     // Update is called once per frame
@@ -25,6 +34,13 @@ public class Basket : MonoBehaviour
         GameObject collidedWith = coll.gameObject;
         if ( collidedWith.tag == "Apple" ) {   
             Destroy( collidedWith );
+            int score = int.Parse(scoreGT.text);
+            score += 100;
+            scoreGT.text = score.ToString();
+            if (score > HighScore.score) {
+                HighScore.score = score;
+ 
+            }
         }
     }
 }
