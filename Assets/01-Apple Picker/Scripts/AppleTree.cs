b1caaf7ed02e003faebf94958aebf98d2a1a772e
Klarrc <kevindanaclark@gmail.com>
2023-02-05 19:44:45 -0500

    Script Writing
    Worked a lot on the appletree movement via the script

---
+++ Assets/01-Apple Picker/Scripts/AppleTree.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+ 
+    // Prefab for instantiating apples
+ 
+    public GameObject   applePrefab;
+                    
+    // Speed at which the AppleTree moves
+ 
+    public float        speed = 1f;
+                    
+    // Distance where AppleTree turns around
+ 
+    public float        leftAndRightEdge = 20f;
+                    
+    // Chance that the AppleTree will change di
+ 
+    public float        chanceToChangeDirection = 0.1f;
+                    
+    // Rate at which Apples will be instantiate
+ 
+    public float        secondsBetweenAppleDrop = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Basic Movement
+ 
+        Vector3 pos = transform.position;      
+ 
+        pos.x += speed * Time.deltaTime;       
+ 
+        transform.position = pos;              
+ 
+        // Changing Direction
+    }
+}
