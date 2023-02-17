f553a841ca842b78f0212e2b1fa97f6b6e13760a
Klarrc <kevindanaclark@gmail.com>
2023-02-16 20:43:25 -0500

    BUILD FINALLY CREATED

--- Assets/02-Mission Demolition/Scripts/Slingshot.cs
+++ Assets/02-Mission Demolition/Scripts/Slingshot.cs
@@ -63,6 +63,8 @@ public class Slingshot : MonoBehaviour
             projectileRigidbody.velocity = -mouseDelta * velocityMult;
             FollowCam.POI = projectile;
             projectile = null;
+            MissionDemolition.ShotFired();
+            ProjectileLine.S.poi = projectile;
         }
     }
 
