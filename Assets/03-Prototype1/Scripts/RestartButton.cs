447e9ff683afe8ff379ec07752b0ae262b7ca181
Klarrc <kevindanaclark@gmail.com>
2023-02-23 20:39:31 -0500

    added readme file

--- Assets/03-Prototype1/Scripts/RestartButton.cs
+++ Assets/03-Prototype1/Scripts/RestartButton.cs
@@ -5,7 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class RestartButton : MonoBehaviour
 {
-    // Start is called before the first frame update
     public void OnButtonPress()
     {
         SceneManager.LoadScene("Main-Prototype 1");
